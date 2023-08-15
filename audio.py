7ff0f0bde24f062a6383f639f619f7081d88ee26
Liza <elizaveta.markushkina@yandex.ru>
2023-08-15 13:43:30 +0300

    code

---
+++ audio.py
@@ -0,0 +1,25 @@
+from transformers import BarkModel, AutoProcessor
+import torch
+import scipy
+
+
+def main():
+    pass
+
+
+def text_to_audio(bark_model='suno/bark', voice_preset='v2/ru_speaker_6', text='Something'):
+    model = BarkModel.from_pretrained(bark_model)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    processor = AutoProcessor.from_pretrained(bark_model)
+
+    inputs = processor(text, voice_preset=voice_preset).to(device)
+    audio_array = model.generate(**inputs)
+    audio_array = audio_array.cpu().numpy().squeeze()
+
+    sample_rate = model.generation_config.sample_rate
+    scipy.io.wavfile.write(f"{voice_preset.split('/')[0]}.wav", rate=sample_rate, data=audio_array)
+
+
+if __name__ == '__main__':
+    text_to_audio()
